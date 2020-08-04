e2e025a965ea0a8b085dfd5a7347056ca4083ba7
agatory <agatory@gmail.com>
2020-08-04 15:15:28 +0900

    #update

--- clock.js
+++ clock.js
@@ -2,21 +2,17 @@ const clockContainer = document.querySelector(".js-clock");
 const clocktTitle = clockContainer.querySelector("h1");
 
 function getTime() {
-    const date = new Date();
-    const minutes = date.getMinutes();
-    const hours = date.getHours();
-    const seconds = date.getSeconds();
-    clocktTitle.innerText = `${hours < 10
-        ? `0${hours}`
-        : hours}:${minutes < 10
-            ? `0${minutes}`
-            : minutes}:${seconds < 9
-                ? `0${seconds}`
-                : seconds}`;
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clocktTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 9 ? `0${seconds}` : seconds}`;
 }
 function init() {
-    getTime();
-    setInterval(getTime, 1000);
+  getTime();
+  setInterval(getTime, 1000);
 }
 
 init();
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const title = document.getElementById("title");
 
 function handleClick(event) {
-    title.style.color = "red"
-    console.log(event);
+  title.style.color = "red";
+  console.log(event);
 }
 
 title.addEventListener("click", handleClick);
